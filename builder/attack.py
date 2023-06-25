7c14ae218691edfe9753069f757c6ab5de1298e8
Daniel Kusuma <daniel.gabrielyudha@gmail.com>
2023-06-25 19:57:23 +0200

    add attack handler

--- builder/attack.py
+++ builder/attack.py
@@ -6,15 +6,19 @@ import random
 
 import numpy as np
 import torch
-from robustness.attacks import ATTACKS
+# TODO: this ugly
+import sys
+sys.path.append('../')
+from TUBSRobustCheck.robustness.attacks import ATTACKS
 
 class Attack:
     SUPPORTED_ATTACKS = ["fgsm", "llcm", "metzen", "metzen_uap", "pgd"]
-    def __init__(self, epsilon, model, type="fgsm", target_type='static'):
+    def __init__(self, epsilon, model, type="fgsm", target_type='dynamic', iterations=60):
         self.epsilon = epsilon
         self.model = model
         self.type = type
         self.target_type = target_type
+        self.iterations = iterations
         self._subset_valid()
         self.attack = self._set_attacks()
 
@@ -33,17 +37,18 @@ class Attack:
         if self.type in ["fgsm", "llcm"]:
             attack = self._create_adversarial_attack("fgsm")(epsilon=self.epsilon)
         elif self.type == "metzen":
-            attack = self._create_adversarial_attack(self.type)(epsilon=self.epsilon, iterations=40, target_type=self.target_type, alpha=2 / 255)
+            attack = self._create_adversarial_attack(self.type)(epsilon=self.epsilon, iterations=self.iterations, target_type=self.target_type, alpha=2 / 255)
         elif self.type == "metzen_uap":
             attack = self._create_adversarial_attack(self.type)(epsilon=self.epsilon, iterations=40, target_type=self.target_type, alpha=2 / 255)
         return attack
 
     def _create_adversarial_attack(self, name):
+        # print(ATTACKS) #debugger
         module = importlib.import_module(ATTACKS[name].classpath)
         attack = getattr(module, ATTACKS[name].classname)
         return attack
 
-    def generate(self, x, y):
+    def generate(self, x, y=None):
         with torch.enable_grad():
             if self.type in ["fgsm", "llcm"]:
                 targetted = True if self.type == "llcm" else False
