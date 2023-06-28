3c87e7b8c8d8f70a0092ea08ed5e6aee29cd18c3
Daniel Kusuma <daniel.gabrielyudha@gmail.com>
2023-06-28 15:16:05 +0200

    bugfix

--- builder/util.py
+++ builder/util.py
@@ -1,15 +1,13 @@
 #!/usr/bin/env python
-import numpy as np
-import matplotlib.pyplot as plt
-import torch
-import torchvision.transforms as transforms
-import torchvision.models as models
-import torch.nn as nn
-import cv2
-import sys
 import os
+import cv2
+import numpy as np
+import torch
+import torch.nn as nn
+import torchvision.transforms as transforms
+
+from .attack import attack
 from dataloader.definitions.labels_file import *
-from .attack import Attack
 
 from PerfPredRecV2.models.wrapper import load_model_def
 
@@ -20,7 +18,7 @@ STD = [0.229, 0.224, 0.225]
 mean = torch.tensor(MEAN).reshape(1, -1, 1, 1)
 std = torch.tensor(STD).reshape(1, -1, 1, 1)
 
-HEIGHT, WIDTH = 1024, 2048
+HEIGHT, WIDTH = 150, 500
 preprocess = transforms.Compose([
     transforms.ToPILImage(),
     transforms.Resize((HEIGHT, WIDTH)),
@@ -30,30 +28,72 @@ preprocess = transforms.Compose([
 
 reg_coeff = np.array([0.09334191 -0.6773528  -9.46617536])
 
+# TODO: make an object of this
+# so that it can become more flexible
+def load_model(model_name="SwiftNetRec",
+    backbone="resnet18",
+    rec_decoder="swiftnet",
+    num_class=19,                           # Cityscapes
+    load_seg=False,
+    device=torch.device('cpu'),
+    dataset='cityscapes',
+):
+    config = {
+        'name': model_name,
+        'backbone': backbone,
+        'decoder': rec_decoder,
+        'trained': load_seg,
+        'num_classes': num_class,
+        'dataset': dataset,
+    }
+    print("Model is loaded with following parameters:")
+
+    # Show parameters
+    for k, v in config.items():
+        print(k, ":", v)
+    print("\n")
+
+    # TODO: load_model_def using config dictionary
+    model = load_model_def(model_name, num_class, rec_decoder=rec_decoder)
+    if load_seg:
+        weightspath = os.path.join("builder/weights", model_name.lower(), backbone, 'model.pth')
+        assert os.path.exists(weightspath), f"{weightspath} does not exists."
+        model.load_state_dict(torch.load(weightspath, map_location=device))
+        print("\nSegmentation weights are sucessfully loaded!")
+
+    model = model.to(device)
+    model.eval()
+    return model
+
 def get_miou_estimate(psnr):
     return 0.09334191 * (psnr**2) - 0.6773528 * psnr - 9.46617536
 
-def colorize(tensor, num_classes=18):
+def colorize(tensor, num_classes=19):
     new_tensor = torch.zeros((tensor.shape[0], tensor.shape[1], 3), dtype=torch.uint8)
     for trainid in range(num_classes):
         new_tensor[tensor == trainid] = torch.tensor(trainid2label[trainid].color, dtype=torch.uint8)
     new_tensor[tensor == -100] = torch.tensor(trainid2label[255].color, dtype=torch.uint8)
     return new_tensor
 
-def postprocess(output):
-    def get_mask(logits):
+def postprocess(output, assign_colour=True):
+
+    def get_mask(logits, assign_colour=True):
         _, mask = torch.max(logits, 1)
         mask = mask.squeeze().cpu().numpy()
+        if assign_colour:
+            mask = colorize(mask).numpy() / 255.
         return mask
-    if type(output) is not dict: return get_mask(output)
-    mask = get_mask(output["logits"])
-    mask = colorize(mask).numpy() / 255.
+
+    # output of single head model
+    if type(output) is not dict: return (get_mask(output, assign_colour=assign_colour), None)
+
+    mask = get_mask(output["logits"], assign_colour=assign_colour)
     rec_image = output["image_reconstruction"]
     rec_image = denormalize(rec_image)
     return (mask, rec_image)
 
 def generate_attack(epsilon, model, attack_type="fgsm", iterations=40):
-    return Attack(epsilon, model, type=attack_type, iterations=iterations)
+    return attack.Attack(epsilon, model, type=attack_type, iterations=iterations)
 
 def apply(var, obj):
     return obj(var)
@@ -75,9 +115,33 @@ def denormalize(tensor):
 def display():
     pass
 
-def inference(model):
-    video_path = "videos/tbilisi.mov"
-    cap = cv2.VideoCapture(video_path)
+def play_images_as_video(directory):
+    # Get the list of image filenames in the directory and sort them
+    images = sorted(os.listdir(directory))
+
+    # Read the first image to get dimensions
+    first_image_path = os.path.join(directory, images[0])
+    first_image = cv2.imread(first_image_path)
+    height, width, _ = first_image.shape
+
+    # Create a VideoWriter object to save the video
+    output_video = cv2.VideoWriter('output_video.avi', cv2.VideoWriter_fourcc(*'XVID'), 30, (width, height))
+
+    # Iterate over the images, read each image, and write it to the video
+    for image_name in images:
+        image_path = os.path.join(directory, image_name)
+        image = cv2.imread(image_path)
+        output_video.write(image)
+        cv2.imshow('Video Playback', image)
+        if cv2.waitKey(25) & 0xFF == ord('q'):  # Press 'q' to exit
+            break
+
+    # Release the VideoWriter and close any open windows
+    output_video.release()
+    cv2.destroyAllWindows()
+
+def inference(model, path="builder/videos/tbilisi.mov"):
+    cap = cv2.VideoCapture(path)
     with torch.no_grad():
         while cap.isOpened():
             ret, frame = cap.read()
@@ -91,16 +155,17 @@ def inference(model):
             output = model(input_tensor)
             output = postprocess(output)
             mask = output[0]
-            reconstructed = output[1]
+            if output[1] is not None:
+                reconstructed = output[1]
+                input_tensor = denormalize(input_tensor)
+                psnr = get_psnr(input_tensor, reconstructed)
+                print(f"PSNR: {psnr} dB")
+                print(f"mIoU estimate: {get_miou_estimate(psnr)} %")
+                cv2.imshow("Reconstruction", reconstructed)
+                cv2.moveWindow("Reconstruction", 200, shift_window)
 
-            input_tensor = denormalize(input_tensor)
-            psnr = get_psnr(input_tensor, reconstructed)
-            print(f"PSNR: {psnr} dB")
-            print(f"mIoU estimate: {get_miou_estimate(psnr)} %")
-            cv2.imshow("Reconstruction", reconstructed)
-            cv2.moveWindow("Reconstruction", 0, 200)
             cv2.imshow("Ground truth", frame)
-            cv2.moveWindow("Segmentation", 0, 400)
+            cv2.moveWindow("Segmentation", 400, shift_window)
             cv2.imshow("Segmentation", mask)
             
             if cv2.waitKey(1) & 0xFF == ord('q'):
@@ -178,32 +243,3 @@ def remove_rec_output(x):
     for key in x.keys():
         assert key in ['image_reconstruction', 'logits'], f"x has not the correct keys {x.keys()}"
     return x['logits']
-
-if __name__ == "__main__":
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    model_name = "SwiftNetRec"
-    num_classes = 19
-    backbone = "resnet18"
-    rec_decoder = "swiftnet"
-    model = load_model_def(model_name, num_classes, rec_decoder=rec_decoder)
-    weightspath = os.path.join("weights", model_name.lower(), backbone, 'model.pth')
-    assert os.path.exists(weightspath), f"{weightspath} does not exists."
-    model.load_state_dict(torch.load(weightspath, map_location=device))
-
-    model = model.to(device)
-    model.eval()
-    # inference(model)
-    model = ModelWrapper(model, postprocessing=remove_rec_output)
-    attack = generate_attack(10 / 255, model, attack_type="metzen", iterations=1)
-    sample = single_frame(inference=False)
-    sample_og = denormalize(sample)
-    print(sample_og)
-    # cv2.imshow("Attacked image", sample_og)
-    # cv2.waitKey(0)
-    # cv2.destroyAllWindows()
-    out_attack, _ = attack.generate(sample)
-    print(out_attack)
-    # out_attack = denormalize(out_attack)
-    # cv2.imshow("Attacked image", out_attack)
-    # cv2.waitKey(0)
-    # cv2.destroyAllWindows()
